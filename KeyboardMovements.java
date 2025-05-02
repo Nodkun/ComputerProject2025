fcf9f387836fde1a47eda6ade96b1cac36565c5f
Blazifire <mvcbothra@gmail.com>
2025-05-02 23:51:34 +0530

    VID 2 , PART 2

---
+++ KeyboardMovements.java
@@ -0,0 +1,45 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyboardMovements implements KeyListener
+{
+    public boolean upPressed , downPressed , leftPressed , rightPressed; //boolean variables to check if the keys are pressed
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+    }
+
+    public void keyPressed(KeyEvent e){
+        //TODO Auto-generated method stub
+        int key = e.getKeyCode(); //get the key that was pressed
+        if (key == KeyEvent.VK_W) { 
+            upPressed = true; //set the upPressed variable to true if the W key is pressed
+        }
+        if (key == KeyEvent.VK_S) { 
+            downPressed = true; //set the downPressed variable to true if the S key is pressed
+        }
+        if (key == KeyEvent.VK_A) { 
+            leftPressed = true; //set the leftPressed variable to true if the A key is pressed
+        }
+        if (key == KeyEvent.VK_D) { 
+            rightPressed = true; //set the rightPressed variable to true if the D key is pressed    
+        }
+    }
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+        
+        int code = e.getKeyCode(); //get the key that was released
+
+        if (key == KeyEvent.VK_W) { 
+            upPressed = false; //set the upPressed variable to true if the W key is pressed
+        }
+        if (key == KeyEvent.VK_S) { 
+            downPressed = false; //set the downPressed variable to true if the S key is pressed
+        }
+        if (key == KeyEvent.VK_A) { 
+            leftPressed = false; //set the leftPressed variable to true if the A key is pressed
+        }
+        if (key == KeyEvent.VK_D) { 
+            rightPressed = false; //set the rightPressed variable to true if the D key is pressed    
+        }
+    }
+}
--- Subpanel.java
+++ Subpanel.java
@@ -17,12 +17,24 @@ public class Subpanel extends JPanel implements Runnable
     final int screenWidth = tileSize * maxScreenCol; //768 pixels
     final int screenHeight = tileSize * maxScreenRow; //576 pixels
 
+    int FPS = 60; //frames per second
+    
+    KeyboardMovements keyH = new KeyboardMovements(); //create an instance of the KeyboardMovements class
+
     Thread gameThread;
+
+    //Set player's default position
+    int playerX = 100; //x position of the player
+    int playerY = 100; //y position of the player
+    int playerSpeed = 4; //speed of the player
+
     public Subpanel()
     {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black); //background color
         this.setDoubleBuffered(true); //for better performance
+        this.addKeyListener(keyH); //add the key listener to the panel
+        this.setFocusable(true);
     }
     public void startGameThread()
     {
@@ -35,7 +47,8 @@ public class Subpanel extends JPanel implements Runnable
         // TODO Auto-generated method stub
         while(gameThread != null)
         {
-           // System.out.println("The game loop is running");
+           long currentTime = System.nanoTime(); //get the current time in nanoseconds
+            // System.out.println("The game loop is running");
            // 1 UPDATE: update information such as character postions
            update();
            // 2 DRAW: draw the screen with the updated information
@@ -46,13 +59,29 @@ public class Subpanel extends JPanel implements Runnable
     public void update()
     {
         //update the game
+        if(keyH.upPressed == true) //if the up key is pressed
+        {
+            playerY -= playerSpeed; //move the player up
+        }
+        else if(keyH.downPressed == true) //if the down key is pressed
+        {
+            playerY += playerSpeed; //move the player down
+        }
+        else if (keyH.leftPressed == true) //if the left key is pressed
+        {
+            playerX -= playerSpeed; //move the player left
+        }
+        else if(keyH.rightPressed == true) //if the right key is pressed
+        {
+            playerX += playerSpeed; //move the player right
+        }
     } 
     public void paintComponent(Graphics g)
     {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g; //for better performance
         g2.setColor(Color.white); //set color to white
-        g2.fillRect(100, 100, tileSize, tileSize); //draw a rectangle at (0,0) with width and height of tileSize
+        g2.fillRect(playerX, playerY, tileSize, tileSize); //draw a rectangle at (0,0) with width and height of tileSize
         g2.dispose(); //dispose of the graphics object to free up resources
     }
-}
+} 
