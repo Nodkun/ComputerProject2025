b9065a19ee9cfcbef54146ded32b7938c040ca2e
Blazifire <mvcbothra@gmail.com>
2025-05-02 18:58:43 +0530

    Used Scanner in this commit and it works

--- Project.java
+++ Project.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
 public class Project
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is the objective of the project:");
+        String objective = scanner.nextLine();
+        System.out.println("Objective is: " +objective);
     }
 }
